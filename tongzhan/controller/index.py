d67f8a1149a2bfef86ddb476b0d86079556f5b0b
郭志肖 <guozhixiao@styd.cn>
2020-08-12 17:47:53 +0800

    修改

--- tongzhan/controller/index.py
+++ tongzhan/controller/index.py
@@ -4,38 +4,40 @@ import logging
 import time
 import hashlib
 import cv2
+from .controller import Controller
 logger = logging.getLogger(__name__)
 #
 #
-# class IndexController(Controller):
-#
-#     def __init__(self):
-#         super.__init__(self)
-#
-#     def face(self):
-#         face_image = face_recognition.load_image_file('/Users/guozhixiao/Downloads/timg (2).jpeg')
-#         # 提取人脸编码 人脸的信息
-#         face_encodings = face_recognition.face_encodings(face_image)
-#         print(face_encodings)
-#
-#         # 人脸坐标位置 只取脸
-#         face_locations = face_recognition.face_locations(face_image)
-#
-#         # 数据库人脸
-#         face2 = face_encodings  # 查询数据库人脸循环做对比
-#         print(type(face_encodings))
-#         for i in face_encodings:
-#             res = face_recognition.compare_faces([i], face2, tolerance=0.8)
-#             print(res)
-#             if res == [True]:
-#                 print(111)
-#             elif res == [False]:
-#                 print(123)
-#
-#         # 人脸比较
-#         results = face_recognition.compare_faces([i],face2, tolerance=0.8)
-#         # print(results)
-# md5加密
+class IndexController(Controller):
+
+    def __init__(self):
+        super.__init__(self)
+    def add(self):
+        pass
+
+    def face(self):
+        face_image = face_recognition.load_image_file('/Users/guozhixiao/Downloads/timg (2).jpeg')
+        # 提取人脸编码 人脸的信息
+        face_encodings = face_recognition.face_encodings(face_image)
+        print(face_encodings)
+
+        # 人脸坐标位置 只取脸
+        face_locations = face_recognition.face_locations(face_image)
+
+        # 数据库人脸
+        face2 = face_encodings  # 查询数据库人脸循环做对比
+        print(type(face_encodings))
+        for i in face_encodings:
+            res = face_recognition.compare_faces([i], face2, tolerance=0.8)
+            print(res)
+            if res == [True]:
+                print(111)
+            elif res == [False]:
+                print(123)
+
+        # 人脸比较
+        results = face_recognition.compare_faces([i],face2, tolerance=0.8)
+        # print(results)
 def md5(password):
     md5 = hashlib.md5()
     # 进行加密，python2可以给字符串加密，python3只能给字节加密
